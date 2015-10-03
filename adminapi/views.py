dba620b514e458b1dd0eaf4f47d916fbf559467f
Pradip Caulagi <pradip.caulagi@fyndiq.com>
2015-10-03 16:50:52 +0200

    Working version of index view that lists all the models registered in admin

--- adminapi/views.py
+++ adminapi/views.py
@@ -2,11 +2,24 @@
 
 import adminapi
 
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.contrib.admin.sites import site as admin_site
 from django.views.generic import View
 
 class IndexView(View):
 
+    def sanitize_response(self, json):
+        """Remove attributes from the response that can't be json serialized"""
+
+        json.pop("site_header")
+        json.pop("site_title")
+        json.pop("title")
+        for app in json.get("app_list"):
+            app.pop("name")
+            for model in app.get("models"):
+                model.pop("name")
+        return json
+
     def get(self, request):
-        return HttpResponse("{0} - {1}".format(
-            adminapi.__name__, adminapi.__version__))
+        html = admin_site.index(request)
+        return JsonResponse(self.sanitize_response(html.context_data))
